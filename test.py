da88682f6adfd594da8062457716842ec7fc4c22
Your Name <you@example.com>
2022-07-17 13:44:06 +0200

    add generated test data script

---
+++ test.py
@@ -0,0 +1,12 @@
+import os
+import json
+
+DIRECTORY = '/home/antho/Documents/Master/fdi-leex/ia/archive/chest_xray/test/NORMAL'
+with open('test.jsonl', 'a+') as file:
+    for dirname, _, filenames in os.walk(DIRECTORY):
+        for filename in filenames:
+            path = os.path.join(DIRECTORY, dirname, filename)
+            data = {"content": "gs://anthonytla2/test_normal/"+filename, "mimeType": "image/jpeg"}
+            file.write('\n')
+            file.write(json.dumps(data))
+

520f59ba398d51f9658d746cb7fc6f6497adeebb
Your Name <you@example.com>
2022-07-19 21:26:35 +0200

    add confusion_matrix

--- find_score.py
+++ find_score.py
@@ -1,10 +1,14 @@
- import json
+import json
 
-    with open("azure.json") as jsonFile:
-        data = json.load(jsonFile)
-        jsonData = data["emp_details"]
-        for x in jsonData:
-            keys = x.keys()
-            print(keys)
-            values = x.values()
-            print(values)
+keys = []
+
+with open("azure.json") as jsonFile:
+    mean = 0
+    nbTotal = 0
+    data = json.load(jsonFile)
+    jsonData = data["documents"]
+    for key in jsonData[0]["entities"]:
+        mean+=key["confidenceScore"]
+        nbTotal = nbTotal + 1
+        #if key.keys()[0] == "confidence": print("IN")
+    print(mean/nbTotal)
--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import os
 import json
 
-DIRECTORY = '/home/antho/Documents/Master/fdi-leex/ia/archive/chest_xray/test/NORMAL'
+DIRECTORY = '/home/antho/Documents/Master/fdi-leex/ia/archive/chest_xray/test/PNEUMONIA'
 with open('test.jsonl', 'a+') as file:
     for dirname, _, filenames in os.walk(DIRECTORY):
         for filename in filenames:
             path = os.path.join(DIRECTORY, dirname, filename)
-            data = {"content": "gs://anthonytla2/test_normal/"+filename, "mimeType": "image/jpeg"}
+            data = {"content": "gs://anthonytla2/test_pneumonia/"+filename, "mimeType": "image/jpeg"}
             file.write('\n')
             file.write(json.dumps(data))
 
